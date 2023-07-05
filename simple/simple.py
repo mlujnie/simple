* fixed a bug for the min_flux (mean_intensity_per_redshift) calculation * added the option to bin_scipy to output the number of modes.

simple/simple.py
@@ -914,7 +914,7 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
             else:
                 self.obs_mask = obs_mask
             print((self.obs_mask.shape, self.N_mesh))
-            assert (self.obs_mask.shape == self.N_mesh).all()
+            assert (np.array(self.obs_mask.shape) == self.N_mesh).all()
         else:
             self.obs_mask = None
 
@@ -2046,7 +2046,7 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
                 min_flux = min_flux(self.cat["cosmo_redshift"])
                 self.cat["detected"] = np.array(self.cat["flux"] > min_flux)
             elif np.size(min_flux) > 1:
-                assert (np.shape(min_flux) == self.N_mesh).all()
+                assert (np.array(np.shape(min_flux)) == self.N_mesh).all()
                 self.cat["detected"] = apply_selection_function_by_position(self.cat['Position'],
                                                                             self.cat['flux'].to(
                                                                                 u.erg/u.s/u.cm**2).value,
@@ -2641,7 +2641,7 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
                 # if it is not a function, but a scalar or a numpy array we can skip the callable.
                 if np.size(self.sigma_noise) > 1:
                     try:
-                        assert (self.sigma_noise.shape == self.N_mesh).all()
+                        assert (np.array(self.sigma_noise.shape) == self.N_mesh).all()
                     except AssertionError:
                         logging.error(
                             f"{self.sigma_noise.shape=} but {self.N_mesh=}")
@@ -2705,7 +2705,7 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
             np.size(self.sky_intensity_mesh[self.sky_intensity_mesh < 0.])/np.prod(self.N_mesh)))
         return self.sky_intensity_mesh.to(self.mean_intensity)
 
-    def bin_scipy(self, pkspec):
+    def bin_scipy(self, pkspec, return_nmodes=False):
         """
         Perform binning of the power spectrum.
 
@@ -2713,6 +2713,9 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
         ----------
         pkspec : ndarray
             The power spectrum mesh to be binned.
+        return_nmodes: bool
+            Set to true if you want to have the number of modes in each bin returned.
+            Default: False.
 
         Returns
         -------
@@ -2731,16 +2734,8 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
         pkspec = np.concatenate(np.concatenate(pkspec))
 
         logging.info("Calculating summary statistics.")
-        (
-            mean_k,
-            monopole,
-            quadrupole,
-        ) = bin_scipy(pkspec, k_bins, kspec, muspec)
-        return (
-            mean_k,
-            monopole,
-            quadrupole,
-        )
+        return_tuple = bin_scipy(pkspec, k_bins, kspec, muspec, return_nmodes=return_nmodes) #(mean_k,monopole,quadrupole,(nmodes))
+        return return_tuple
 
     def _get_prepared_intensity_mesh(self, sky_subtraction):
         """
@@ -3036,7 +3031,16 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
             raise ValueError(
                 "Invalid 'tracer' argument: must be either 'intensity' or 'n_gal'."
             )
-
+        
+        # if min_flux is a 3D array with the same shape as N_mesh,
+        # return the mean intensity of the mesh as a function of LOS distance
+        # because we can't make this calculation for each cell.
+        if (not callable(self.min_flux)):
+            if (np.array(np.shape(self.min_flux)) == self.N_mesh).all():
+                mean_intensity_per_redshift = np.mean(
+                    self.intensity_mesh, axis=(1, 2))
+                return mean_intensity_per_redshift
+            
         if galaxy_selection == "all":
             if tracer == "intensity":
                 integrated = quad(
@@ -3062,11 +3066,6 @@ Plot plt.loglog(Ls, lim.luminosity_function(Ls)) in a reasonable range to check 
                 raise ValueError(
                     "Invalid 'tracer' argument: must be either 'intensity' or 'n_gal'."
                 )
-
-        elif (not callable(self.min_flux)) * (np.shape(self.min_flux) == self.N_mesh).all():
-            mean_intensity_per_redshift = np.mean(
-                self.intensity_mesh, axis=(1, 2))
-            return mean_intensity_per_redshift
 
         elif (galaxy_selection == "detected") or (galaxy_selection == "undetected"):
             mean_luminosity_density = []

simple/tools_python.py
@@ -5,13 +5,14 @@ import psutil
 import os
 import pmesh
 import logging
-#from yaml import load
+# from yaml import load
 from astropy.io.misc import yaml
 from scipy.stats import binned_statistic_2d, binned_statistic
 from scipy.special import legendre, j1
 from scipy.interpolate import interp1d
 
-def log_interp1d(xx, yy, kind='linear',bounds_error=False,fill_value='extrapolate'):
+
+def log_interp1d(xx, yy, kind='linear', bounds_error=False, fill_value='extrapolate'):
     """
     Logarithmic interpolation accepting linear quantities as input (transformed within the function).
 
@@ -34,8 +35,8 @@ def log_interp1d(xx, yy, kind='linear',bounds_error=False,fill_value='extrapolat
         A callable function that performs logarithmic interpolation on input values.
 
     """
-    
-    ind = np.where(yy>0)
+
+    ind = np.where(yy > 0)
     try:
         logx = np.log10(xx[ind].value)
     except:
@@ -44,17 +45,19 @@ def log_interp1d(xx, yy, kind='linear',bounds_error=False,fill_value='extrapolat
         logy = np.log10(yy[ind].value)
     except:
         logy = np.log10(yy[ind])
-    lin_interp = interp1d(logx, logy, kind=kind,bounds_error=bounds_error,fill_value=fill_value)
-    
-    log_interp = lambda zz: np.power(10.0, lin_interp(np.log10(zz)))
+    lin_interp = interp1d(logx, logy, kind=kind,
+                          bounds_error=bounds_error, fill_value=fill_value)
+
+    def log_interp(zz): return np.power(10.0, lin_interp(np.log10(zz)))
 
     return log_interp
+
 
 def yaml_file_to_dictionary(filename):
     """ Opens a yaml file and returns a dictionary from its contents. """
     with open(filename, "r") as stream:
         data = yaml.load(stream)
-        #data = yaml_stream_to_dictionary(stream)
+        # data = yaml_stream_to_dictionary(stream)
     return data
 
 
@@ -192,6 +195,7 @@ def get_checker_mask(N_mesh, N_cells=1):
     obs_mask = np.array([obs_mask_2d for i in range(N_mesh[0])])
     return obs_mask
 
+
 def bin_scipy(pkspec, k_bins, kspec, muspec, lmax=2, return_nmodes=False):
     """
     Calculates the monopole and quadrupole (if lmax==2) power spectrum and mean k in the given k bins.
@@ -244,7 +248,8 @@ def bin_scipy(pkspec, k_bins, kspec, muspec, lmax=2, return_nmodes=False):
     logging.info("Calculated mean_k.")
 
     if return_nmodes:
-        nmodes, k_edge, bin_number = binned_statistic(np.ones(kspec.shape), kspec, bins=k_bins, statistic='sum')
+        nmodes, k_edge, bin_number = binned_statistic(
+            kspec, kspec, bins=k_bins, statistic='count')
         return mean_k, monopole, 5 * quadrupole, nmodes
     else:
         return (
@@ -252,6 +257,7 @@ def bin_scipy(pkspec, k_bins, kspec, muspec, lmax=2, return_nmodes=False):
             monopole,
             5 * quadrupole,
         )
+
 
 def bin_Pk_2d(pkspec, k_bins, k_par, k_perp):
     """
@@ -281,6 +287,7 @@ def bin_Pk_2d(pkspec, k_bins, k_par, k_perp):
     )
     logging.info("Calculated P_k_2d.")
     return P_k_2d
+
 
 def bin_Pk_mu(pkspec, k_bins, kspec, muspec, mu_bins):
     """
@@ -325,8 +332,8 @@ def bin_Pk_mu(pkspec, k_bins, kspec, muspec, mu_bins):
     )
     logging.info("Calculated mean mu (2d).")
     return (mean_k_2d,
-        mean_mu_2d,
-        P_k_mu,)
+            mean_mu_2d,
+            P_k_mu,)
 
 
 def jinc(x):
@@ -336,6 +343,7 @@ def jinc(x):
 
     """
     return np.where(x != 0, j1(x) / x, 0.5)
+
 
 def make_map(m, Nmesh, BoxSize, type="real"):
     """