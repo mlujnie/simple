dc814a3e8525a3562e34ffec4373262466753a0e
Maja Lujan Niemeyer <44762816+mlujnie@users.noreply.github.com>
2023-07-12 08:24:06 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,4 +69,4 @@ source_suffix = ['.rst', '.md']
 autodoc_mock_imports = ['astropy', 'numpy', 'dask', 'scipy', 
                         'h5py', 'pmesh', 'psutil', 'random',
                         'os', 'logging', 'functools',
-                       'copy', 'sys', 'Cython']#, 'simple', 'tools'] #, 'tools_python','simple', 
+                       'copy', 'sys']#, 'Cython']#, 'simple', 'tools'] #, 'tools_python','simple', 
