aef7553c7a290e276b82d1c6cdb73780a9be13d1
Maja Lujan Niemeyer <maja_n@login1.ls6.tacc.utexas.edu>
2025-04-15 06:28:25 -0500

    Changed version to 1.0.1 with new changes.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     name='simple',
-    version='1.0.0',    
+    version='1.0.1',    
     description='Simple Intensity Map Producer for Line Emission',
     url='https://github.com/mlujnie/simple',
     author='Maja Lujan Niemeyer',
